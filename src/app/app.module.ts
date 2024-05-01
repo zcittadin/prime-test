1d3a0e98e4baa3fb038c7f6cd20372e24147bccf
zcittadin <z_cittadin@hotmail.com>
2024-05-01 16:11:46 -0300

    Commit inicial.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { ButtonModule } from 'primeng/button';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { DialogModule } from 'primeng/dialog';
+import { DividerModule } from 'primeng/divider';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { TableModule } from 'primeng/table';
+
+import { ConfirmationService } from 'primeng/api';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeService } from './service/employee.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +24,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    DialogModule,
+    DividerModule,
+    TableModule,
+    ButtonModule,
+    ConfirmDialogModule,
+    InputTextModule,
+    InputNumberModule
   ],
-  providers: [],
+  providers: [EmployeeService, ConfirmationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/model/employee.ts
@@ -0,0 +1,6 @@
+export class Employee {
+    id: number = 0;
+    firstName: string = '';
+    lastName: string = '';
+    salary: number = 0;
+}
